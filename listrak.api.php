13835a378146bfd5ccbd57c85e8399ab7d34ff3c
David Morrison <shalompisteuo@gmail.com>
2015-02-17 15:23:56 -0500

    Adding module files

---
+++ listrak.api.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * @file
+ * Hooks provided by the listrak module.
+ *
+ * @author Tom Kirkpatrick (mrfelton), www.systemseed.com
+ */
+
+/**
+ * Alter the options passed into the Listrak Client.
+ *
+ * @param array $options
+ *   A multideminsional array passed into the Listrak Client.
+ *   $options['options'] is the options passed into the SoapClient.
+ */
+function hook_listrak_client_options_alter(&$options) {
+  $options['options']['soap_version'] = SOAP_1_1;
+}
+
